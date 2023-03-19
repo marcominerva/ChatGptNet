b9cce9ebb5f40172aac0ad1e02da2b21c19034bd
Marco Minerva <marco.minerva@gmail.com>
2023-03-19 12:13:06 +0100

    Documentation update #19

--- src/ChatGptNet/IChatGptClient.cs
+++ src/ChatGptNet/IChatGptClient.cs
@@ -13,7 +13,7 @@ public interface IChatGptClient
     /// </summary>
     /// <param name="message">The system message.</param>
     /// <returns>The new Conversation Id.</returns>
-    /// <remarks>This method creates a new conversation with a system message and a random Conversation Id. Then, call <seealso cref="AskAsync(Guid, string, CancellationToken)"/> with this Conversation Id to start the actual conversation.</remarks>
+    /// <remarks>This method creates a new conversation with a system message and a random Conversation Id. Then, call <seealso cref="AskAsync(Guid, string, CancellationToken)"/> with this Id to start the actual conversation.</remarks>
     /// <exception cref="ArgumentNullException"><paramref name="message"/> is <see langword="null"/>.</exception>
     Task<Guid> SetupAsync(string message)
         => SetupAsync(Guid.NewGuid(), message);
@@ -23,7 +23,7 @@ public interface IChatGptClient
     /// </summary>
     /// <param name="conversationId">The unique identifier of the conversation, used to automatically retrieve previous messages in the chat history.</param>
     /// <param name="message">The system message.</param>
-    /// <remarks>This method creates a new conversation, with a system message and the given Conversation Id. If a conversation with this Id already exists, it will be automatically cleared. Then, call <seealso cref="AskAsync(Guid, string, CancellationToken)"/> with the same Conversation Id to start the actual conversation.</remarks>
+    /// <remarks>This method creates a new conversation, with a system message and the given <paramref name="conversationId"/>. If a conversation with this Id already exists, it will be automatically cleared. Then, call <seealso cref="AskAsync(Guid, string, CancellationToken)"/> with the same <paramref name="conversationId"/> to start the actual conversation.</remarks>
     /// <exception cref="ArgumentNullException"><paramref name="message"/> is <see langword="null"/>.</exception>
     Task<Guid> SetupAsync(Guid conversationId, string message);
 
@@ -33,7 +33,7 @@ public interface IChatGptClient
     /// <param name="message">The message.</param>
     /// <param name="cancellationToken">A <seealso cref="CancellationToken"/> that can be used by other objects or threads to receive notice of cancellation.</param>
     /// <returns>The chat completion response.</returns>
-    /// <remarks>This method automatically starts a new conservation with a random Conversation Id, that will be returned in the <see cref="ChatGptResponse"/>. Subsequent calls to this method must provide the same Conversation Id, so that previous messages will be automatically used to continue the conversation.</remarks>
+    /// <remarks>This method automatically starts a new conservation with a random Conversation Id, that will be returned in the <see cref="ChatGptResponse"/>. Subsequent calls to this method must provide the same <see cref="ChatGptResponse.ConversationId"/> value, so that previous messages will be automatically used to continue the conversation.</remarks>
     /// <exception cref="ArgumentNullException"><paramref name="message"/> is <see langword="null"/>.</exception>
     /// <exception cref="ChatGptException">An error occurred while calling the API and the <seealso cref="ChatGptOptions.ThrowExceptionOnError"/> is <see langword="true"/>.</exception>
     /// <seealso cref="ChatGptRequest"/>
