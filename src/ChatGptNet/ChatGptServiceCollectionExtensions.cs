d544e02ddb3d4bef2a7c45e5ebc0df924912a028
Marco Minerva <marco.minerva@gmail.com>
2023-03-20 10:34:05 +0100

    Add dynamic options configuration #20

--- src/ChatGptNet/ChatGptServiceCollectionExtensions.cs
+++ src/ChatGptNet/ChatGptServiceCollectionExtensions.cs
@@ -9,12 +9,12 @@ namespace ChatGptNet;
 public static class ChatGptServiceCollectionExtensions
 {
     /// <summary>
-    /// Registers <see cref="ChatGptClient"/> with the specified options.
+    /// Registers a <see cref="ChatGptClient"/> instance with the specified options.
     /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
     /// <param name="setupAction">The <see cref="Action{ChatGptOptions}"/> to configure the provided <see cref="ChatGptOptions"/>.</param>
     /// <returns>A reference to this instance after the operation has completed.</returns>
-    /// <remarks>This methods automatically adds a <see cref="MemoryCache"/> that is used to save chat messages for completion.</remarks>
+    /// <remarks>This method automatically adds a <see cref="MemoryCache"/> that is used to save chat messages for completion.</remarks>
     /// <seealso cref="ChatGptOptions"/>
     /// <see cref="MemoryCacheServiceCollectionExtensions.AddMemoryCache(IServiceCollection)"/>
     public static IServiceCollection AddChatGpt(this IServiceCollection services, Action<ChatGptOptions> setupAction)
@@ -30,15 +30,57 @@ public static class ChatGptServiceCollectionExtensions
 
         services.AddHttpClient<IChatGptClient, ChatGptClient>(client =>
         {
-            client.BaseAddress = new Uri("https://api.openai.com/v1/");
-            client.DefaultRequestHeaders.Authorization = new("Bearer", options.ApiKey);
-
-            if (!string.IsNullOrWhiteSpace(options.Organization))
-            {
-                client.DefaultRequestHeaders.Add("OpenAI-Organization", options.Organization);
-            }
+            ConfigureHttpClient(client, options);
         });
 
         return services;
+    }
+
+    /// <summary>
+    /// Registers a <see cref="ChatGptClient"/> instance using dynamic options.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add services to.</param>
+    /// <param name="setupAction">The <see cref="Action{IServiceProvider, ChatGptOptions}"/> to configure the provided <see cref="ChatGptOptions"/>.</param>
+    /// <returns>A reference to this instance after the operation has completed.</returns>
+    /// <remarks>Use this this method if it is necessary to dynamically set options like <see cref="ChatGptOptions.ApiKey"/> (for example, using other services via dependency injection).
+    /// This method automatically adds a <see cref="MemoryCache"/> that is used to save chat messages for completion.
+    /// </remarks>
+    /// <seealso cref="ChatGptOptions"/>
+    /// <seealso cref="IServiceProvider"/>
+    /// <see cref="MemoryCacheServiceCollectionExtensions.AddMemoryCache(IServiceCollection)"/>
+    public static IServiceCollection AddChatGpt(this IServiceCollection services, Action<IServiceProvider, ChatGptOptions> setupAction)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(setupAction);
+
+        var options = new ChatGptOptions();
+        services.AddTransient(provider =>
+        {
+            setupAction.Invoke(provider, options);
+            return options;
+        });
+
+        services.AddMemoryCache();
+
+        services.AddHttpClient<IChatGptClient, ChatGptClient>((provider, client) =>
+        {
+            using var scope = provider.CreateScope();
+            var options = scope.ServiceProvider.GetRequiredService<ChatGptOptions>();
+
+            ConfigureHttpClient(client, options);
+        });
+
+        return services;
+    }
+
+    private static void ConfigureHttpClient(HttpClient client, ChatGptOptions options)
+    {
+        client.BaseAddress = new Uri("https://api.openai.com/v1/");
+        client.DefaultRequestHeaders.Authorization = new("Bearer", options.ApiKey);
+
+        if (!string.IsNullOrWhiteSpace(options.Organization))
+        {
+            client.DefaultRequestHeaders.Add("OpenAI-Organization", options.Organization);
+        }
     }
 }
