aa5e7d020ab7c3beba2595b2882e38b981a6ad0a
Marco Minerva <marco.minerva@gmail.com>
2023-03-22 09:32:56 +0100

    Renamed method #32

--- src/ChatGptNet/Extensions/ChatGptResponseExtensions.cs
+++ src/ChatGptNet/Extensions/ChatGptResponseExtensions.cs
@@ -10,13 +10,13 @@ namespace ChatGptNet.Extensions;
 public static class ChatGptResponseExtensions
 {
     /// <summary>
-    /// Returns an <see cref="IAsyncEnumerable{T}"/> that allows to enumerate all the response messages, each of them containing a partial delta.
+    /// Returns an <see cref="IAsyncEnumerable{T}"/> that allows to enumerate all the partial message deltas.
     /// </summary>
     /// <param name="responses">The source <see cref="IAsyncEnumerable{ChatGptResponse}"/>.</param>
-    /// <returns>An <see cref="IAsyncEnumerable{T}"/> that allows to enumerate all the response messages, each of them containing a partial delta.</returns>
+    /// <returns>An <see cref="IAsyncEnumerable{T}"/> that allows to enumerate all the partial message deltas.</returns>
     /// <seealso cref="ChatGptResponse"/>
     [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "This method returns an IAsyncEnumerable")]
-    public static async IAsyncEnumerable<string> AsMessages(this IAsyncEnumerable<ChatGptResponse> responses)
+    public static async IAsyncEnumerable<string> AsDeltas(this IAsyncEnumerable<ChatGptResponse> responses)
     {
         await foreach (var response in responses)
         {
