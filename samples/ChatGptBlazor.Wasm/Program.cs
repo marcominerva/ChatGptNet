16d90f9a0a451f45446a46790c46d1929ebbc4a3
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2023-03-24 15:33:30 +0100

    basic sample for Blazor WASM

---
+++ samples/ChatGptBlazor.Wasm/Program.cs
@@ -0,0 +1,22 @@
+using ChatGptBlazor.Wasm;
+using ChatGptNet;
+using ChatGptNet.Models;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddChatGpt(options =>
+{
+    options.ApiKey = "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";
+    options.Organization = null;    // Optional
+    options.DefaultModel = ChatGptModels.Gpt35Turbo;  // Default: ChatGptModels.Gpt35Turbo
+    options.MessageLimit = 16;  // Default: 10
+    options.MessageExpiration = TimeSpan.FromMinutes(5);    // Default: 1 hour
+});
+
+await builder.Build().RunAsync();
