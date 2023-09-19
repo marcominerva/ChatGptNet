363e084430e6bf951e63bce066fcaf9746ccc1e4
Marco Minerva <marco.minerva@gmail.com>
2023-09-19 10:13:46 +0200

    Documentation update #117

--- src/ChatGptNet/IChatGptBuilderExtensions.cs
+++ src/ChatGptNet/IChatGptBuilderExtensions.cs
@@ -9,13 +9,12 @@ namespace ChatGptNet;
 public static class IChatGptBuilderExtensions
 {
     /// <summary>
-    /// Uses a custom cache implementation for conversation handling.
+    /// Defines a custom cache implementation for conversation handling.
     /// </summary>
     /// <typeparam name="TImplementation">The implementation of <see cref="IChatGptCache"/> to use.</typeparam>
     /// <param name="builder">The <see cref="IChatGptBuilder"/> object to configure.</param>
     /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service.</param>
     /// <returns>The <see cref="IChatGptBuilder"/> to further customize ChatGPT.</returns>
-    /// <remarks><typeparamref name="TImplementation"/> is registered as singleton.</remarks>
     /// <seealso cref="IChatGptBuilder"/>
     /// <seealso cref="IChatGptCache"/>
     public static IChatGptBuilder WithCache<TImplementation>(this IChatGptBuilder builder, ServiceLifetime lifetime = ServiceLifetime.Singleton)
