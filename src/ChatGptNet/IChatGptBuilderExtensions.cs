0b35acf8cfd90909e2ab50f213f251b7adb7fcd9
Marco Minerva <marco.minerva@gmail.com>
2023-09-19 10:06:16 +0200

    Add Service Lifetime to cache registration #117

--- src/ChatGptNet/IChatGptBuilderExtensions.cs
+++ src/ChatGptNet/IChatGptBuilderExtensions.cs
@@ -13,16 +13,17 @@ public static class IChatGptBuilderExtensions
     /// </summary>
     /// <typeparam name="TImplementation">The implementation of <see cref="IChatGptCache"/> to use.</typeparam>
     /// <param name="builder">The <see cref="IChatGptBuilder"/> object to configure.</param>
+    /// <param name="lifetime">The <see cref="ServiceLifetime"/> of the service.</param>
     /// <returns>The <see cref="IChatGptBuilder"/> to further customize ChatGPT.</returns>
     /// <remarks><typeparamref name="TImplementation"/> is registered as singleton.</remarks>
     /// <seealso cref="IChatGptBuilder"/>
     /// <seealso cref="IChatGptCache"/>
-    public static IChatGptBuilder WithCache<TImplementation>(this IChatGptBuilder builder)
+    public static IChatGptBuilder WithCache<TImplementation>(this IChatGptBuilder builder, ServiceLifetime lifetime = ServiceLifetime.Singleton)
         where TImplementation : class, IChatGptCache
     {
         ArgumentNullException.ThrowIfNull(builder);
 
-        builder.Services.Replace(ServiceDescriptor.Singleton<IChatGptCache, TImplementation>());
+        builder.Services.Replace(new ServiceDescriptor(typeof(IChatGptCache), typeof(TImplementation), lifetime));
 
         return builder;
     }
