ab8d289cb2379981363c8d8e7d903a48cf152980
Marco Minerva <marco.minerva@gmail.com>
2024-07-23 12:48:18 +0200

    Update API version and model references
    - Updated Azure OpenAI service API version from `2024-05-01-preview` to `2024-06-01` in various configuration files.
    - Replaced older GPT model versions with `gpt-4o-mini` in `README.md`.
    - Updated package versions in `.csproj` files for improved compatibility and to include new features or bug fixes, affecting `Microsoft.AspNetCore.OpenApi`, `Microsoft.Extensions.Http.Resilience`, and `Microsoft.AspNetCore.Components.WebAssembly`.
    - Enhanced AzureChatGptServiceConfiguration with support for the new API version and updated OpenAIChatGptModels with new model constants.
    - Incremented `Microsoft.Extensions.Configuration.Binder` package version for better stability and performance.

--- src/ChatGptNet/ServiceConfigurations/AzureChatGptServiceConfiguration.cs
+++ src/ChatGptNet/ServiceConfigurations/AzureChatGptServiceConfiguration.cs
@@ -10,7 +10,7 @@ internal class AzureChatGptServiceConfiguration : ChatGptServiceConfiguration
     /// <summary>
     /// The default API version for Azure OpenAI service.
     /// </summary>
-    public const string DefaultApiVersion = "2024-05-01-preview";
+    public const string DefaultApiVersion = "2024-06-01";
 
     /// <summary>
     /// Gets or sets the name of the Azure OpenAI Resource.
@@ -18,7 +18,7 @@ internal class AzureChatGptServiceConfiguration : ChatGptServiceConfiguration
     public string? ResourceName { get; set; }
 
     /// <summary>
-    /// Gets or sets the API version of the Azure OpenAI service (Default: 2024-05-01-preview).
+    /// Gets or sets the API version of the Azure OpenAI service (Default: 2024-06-01).
     /// </summary>
     /// <remarks>
     /// Currently supported versions are:
@@ -54,6 +54,10 @@ internal class AzureChatGptServiceConfiguration : ChatGptServiceConfiguration
     ///   <item>
     ///     <term>2023-02-01</term>
     ///     <description><see href="https://github.com/Azure/azure-rest-api-specs/blob/main/specification/cognitiveservices/data-plane/AzureOpenAI/inference/stable/2024-02-01/inference.json">Swagger spec</see></description>
+    ///   </item>
+    ///   <item>
+    ///     <term>2024-06-01</term>
+    ///     <description><see href="https://github.com/Azure/azure-rest-api-specs/blob/main/specification/cognitiveservices/data-plane/AzureOpenAI/inference/stable/2024-06-01/inference.json">Swagger spec</see></description>
     ///   </item>
     /// </list>
     /// </remarks>
